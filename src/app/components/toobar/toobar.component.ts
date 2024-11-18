93e760b60d5c7c1f617ff3192b7962942f91cd59
m-zeeshan-aly <m.zeeshan.ali0346@gmail.com>
2024-11-18 14:02:19 +0500

    creating new toobar component

---
+++ src/app/components/toobar/toobar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-toobar',
+  standalone: true,
+  imports: [],
+  templateUrl: './toobar.component.html',
+  styleUrl: './toobar.component.scss'
+})
+export class ToobarComponent {
+
+}
