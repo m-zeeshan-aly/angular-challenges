b1ba88e9bbf9e0a79cb1ae58590bb2a136ddf7d1
m-zeeshan-aly <m.zeeshan.ali0346@gmail.com>
2024-11-19 12:59:30 +0500

    Card Component completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { CardComponent } from './components/card/card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ToolbarComponent],
+  imports: [RouterOutlet,ToolbarComponent,CardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-challenges';
 }
-
 
 
