6aaf2b673620d591dba90ead3d7fb728feb3fd08
m-zeeshan-aly <m.zeeshan.ali0346@gmail.com>
2024-11-20 12:31:02 +0500

    A button is created to nevigate back on to of the page

---
+++ src/app/components/top-of-page/top-of-page.component.ts
@@ -0,0 +1,32 @@
+import { ViewportScroller } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'app-top-of-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './top-of-page.component.html',
+  styleUrl: './top-of-page.component.scss'
+})
+export class TopOfPageComponent {
+
+  public isShown =false;
+
+
+  constructor(private viewPortScroller : ViewportScroller){
+
+
+   }
+
+   @HostListener('window:scroll') onWindowScroll(){
+    console.log(123456);
+    const yCoordinate = this.viewPortScroller.getScrollPosition()[1];
+    this.isShown = yCoordinate >200;
+   }
+
+   public goToTop():void{
+    this.viewPortScroller.scrollToPosition([0,0]);
+
+   }
+}
+
