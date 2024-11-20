788f0427ca88aec9cb775dfed097119e41fa4ae7
m-zeeshan-aly <m.zeeshan.ali0346@gmail.com>
2024-11-20 14:49:05 +0500

    pipe is loaded via Lazy loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { TopOfPageComponent } from './components/top-of-page/top-of-page.component';
-import { RouteModule } from 'next/dist/server/route-modules/route-module';
+
 
 
 @Component({
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,8 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ServiceDocumentationComponent } from './services/service-documentation/service-documentation.component';
 import { DirectiveDocumentationComponent } from './directives/directive-documentation/directive-documentation.component';
-import { PipeDocumentationComponent } from './pipe/pipe-documentation/pipe-documentation.component';
+
+
 
 export const routes: Routes = [
   {
@@ -15,7 +16,7 @@ export const routes: Routes = [
     path: "directives", component :DirectiveDocumentationComponent
   },
   {
-    path: "pipes", component :PipeDocumentationComponent
+    path: "pipes", loadChildren: () => import("./pipe/pipes/pipes.module").then((m) => m.PipesModule)
   },
 ];
 
